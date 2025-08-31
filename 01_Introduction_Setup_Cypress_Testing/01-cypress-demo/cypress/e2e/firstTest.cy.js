fea27f7ba24e98420625f3f00ff47949407f9ae8
Joshi Sourabha <sourabhjoshi124@gmail.com>
2025-08-31 12:34:07 +0530

    cypress commanads

--- 01_Introduction_Setup_Cypress_Testing/01-cypress-demo/cypress/e2e/firstTest.cy.js
+++ 01_Introduction_Setup_Cypress_Testing/01-cypress-demo/cypress/e2e/firstTest.cy.js
@@ -1,6 +1,11 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('/')
-    cy.title().should('include', 'Cypress')
-  })
+    it('passes', () => {
+        cy.visit('/')
+        cy.title().should('include', 'Cypress')
+
+        cy.title().then(title => {
+            console.log('Page title is:', title)   // Browser/terminal console
+            cy.log('Page title: ' + title)         // Cypress UI log
+        })
+    })
 })
